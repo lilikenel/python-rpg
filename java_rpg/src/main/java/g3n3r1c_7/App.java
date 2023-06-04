27baa4eac69cc5edbd655d42aaafbcc447f27b71
“linel022” <linel022@student.wethinkcode.co.za>
2023-06-04 20:21:02 +0200

    built maven project

---
+++ java_rpg/src/main/java/g3n3r1c_7/App.java
@@ -0,0 +1,11 @@
+package g3n3r1c_7;
+
+
+public final class App {
+    private App() {
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ java_rpg/src/test/java/g3n3r1c_7/AppTest.java
@@ -0,0 +1,18 @@
+package g3n3r1c_7;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
