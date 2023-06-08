e9d029d13336306bcbf3d926f58715fb08517c3a
linel022 <linel022@student.wethinkcode.co.za>
2023-06-08 11:44:18 +0200

    feat(GamePanel): white square can be controlled by keyboard input on black window

---
+++ java_rpg/src/main/java/g3n3r1c_7/GamePanel.java
@@ -0,0 +1,102 @@
+package g3n3r1c_7;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Runnable {
+    
+    final int originalTileSize = 16;
+    final int scale = 3;
+    final int tileSize = originalTileSize * scale; //48x48 px
+
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+
+    final int screenWidth = tileSize * maxScreenCol; //786 px
+    final int screenHeight = tileSize * maxScreenRow; //574 px
+
+    Thread gameThread;
+    KeyHandler keyHandler = new KeyHandler();
+
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+    int FPS = 60;
+
+    public GamePanel() {
+
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true); //better rendering performance
+        this.addKeyListener(keyHandler);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread() {
+
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    /*
+    * the main game loop
+    */
+    @Override
+    public void run() {
+
+        double drawInterval = 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+    
+        while (gameThread != null) {
+
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint(); //calls paintComponent()
+                delta--;
+            }
+            
+        }        
+    }
+
+    public void update() {
+
+        if(keyHandler.upPressed == true) {
+            playerY -= playerSpeed;
+        }
+        else if(keyHandler.downPressed == true) {
+            playerY += playerSpeed;
+        }
+        else if(keyHandler.leftPressed == true) {
+            playerX -= playerSpeed;
+        }
+        else if(keyHandler.rightPressed == true) {
+            playerX += playerSpeed;
+        }
+    }
+
+    /*
+     * Essentially the paintbrush we use to draw on window in Main.java
+     */
+    public void paintComponent(Graphics graphics) {
+
+        super.paintComponent(graphics);
+
+        Graphics2D graphics2D = (Graphics2D) graphics;
+        graphics2D.setColor(Color.white);
+
+        graphics2D.fillRect(playerX, playerY, tileSize, tileSize);
+
+        graphics2D.dispose();
+    }
+}
